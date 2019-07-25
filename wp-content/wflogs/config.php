31315b6357e749b7a0bc891298503d8cdcd97372
AnySupport <college@anysupport.co.uk>
2019-07-26 00:21:58 +0100

    Initial commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1564489108;s:7:"authKey";s:64:"*n{a&J?$jcbhYwLPEPTzA9hB*%$}Y3y^EJTMq)bcoNfqrVB7,G$#rElBuIXh9+DC";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:533;}
